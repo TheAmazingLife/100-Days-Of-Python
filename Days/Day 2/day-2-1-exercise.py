953768f97a6197c6e61332e0563e4c57a342245b
Joaquin <joaquin.san.vargas19@gmail.com>
2022-12-05 23:53:53 -0300

    Day 2 old progress

---
+++ Days/Day 2/day-2-1-exercise.py
@@ -0,0 +1,6 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
