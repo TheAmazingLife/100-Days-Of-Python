55ff799bbf4452e4eacc43e303d5aac4e6c5b20e
Joaquin <joaquin.san.vargas19@gmail.com>
2023-01-04 22:27:28 -0300

    Day 6 End

---
+++ Days/Day 6/Excercises/Maze.py
@@ -0,0 +1,25 @@
+# Interactive Coding Exercises
+# Maze w/debug
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+#bulce while para lograr que tenga una muralla a la derecha
+while front_is_clear():
+    move()
+turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
